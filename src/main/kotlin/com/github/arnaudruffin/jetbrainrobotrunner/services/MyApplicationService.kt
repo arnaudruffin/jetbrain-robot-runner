6e11a3759dd58f565eaad9d93c987dfabc352b96
GitHub Action <action@github.com>
2021-07-30 07:00:24 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/arnaudruffin/jetbrainrobotrunner/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.arnaudruffin.jetbrainrobotrunner.services
+
+import com.github.arnaudruffin.jetbrainrobotrunner.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
--- src/main/kotlin/com/github/arnaudruffin/jetbrainrobotrunner/services/MyProjectService.kt
+++ src/main/kotlin/com/github/arnaudruffin/jetbrainrobotrunner/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.arnaudruffin.jetbrainrobotrunner.services
 
+import com.github.arnaudruffin.jetbrainrobotrunner.MyBundle
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
 
 class MyProjectService(project: Project) {
 
